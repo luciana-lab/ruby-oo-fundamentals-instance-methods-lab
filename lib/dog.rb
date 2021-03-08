cb7bd89465cbb3b244ee2fd50f7dc15a5f099e0b
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-08 18:48:57 -0500

    commit tree 0e6bb9f2d29ea9e73680ea912ed44aedf52ae42a

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+#Dog
+    #is defined
+#bark
+  #responds to the #bark instance method (FAILED - 2)
+  #puts "Woof!" (FAILED - 3)
+#sit
+  #responds to the #sit instance method (FAILED - 4)
+  #puts "The Dog is sitting" (FAILED - 5)
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+#Person
+  #is defined 
+  #talk
+    #responds to the #talk instance method 
+    #puts "Hello World!"
+  #walk
+    #responds to the #walk instance method 
+    #puts "The Person is walking"
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
